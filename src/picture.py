05c993997be64db416c6a84a60e47190c782b09c
diego <dcervantesapa@unsa.edu.pe>
2026-05-16 22:57:27 -0500

    añadimos funcion negative() en picture.py

--- src/picture.py
+++ src/picture.py
@@ -21,11 +21,21 @@ class Picture:
 
   def horizontalMirror(self):
     """ Devuelve el espejo horizontal de la imagen """
-    # Invierte el orden de las filas (de arriba a abajo)
     return Picture(self.img[::-1])
 
   def negative(self):
-    return Picture(None)
+    """ Devuelve un negativo de la imagen """
+    #Creamos una lista vacía para almacenar las nuevas filas con colores invertidos
+    negativo = []
+    
+    #Recorremos fila por fila el atributo interno self.img
+    for fila in self.img:
+        #Para cada carácter en la fila, llamamos a _invColor y los unimos de nuevo en un string
+        nueva_fila = "".join([self._invColor(caracter) for caracter in fila])
+        negativo.append(nueva_fila)
+        
+    #Retornamos un nuevo objeto Picture con la representación modificada
+    return Picture(negativo)
 
   def join(self, p):
     return Picture(None)
