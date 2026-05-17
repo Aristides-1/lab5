f2e1b66450ecee1b3a03c7b4eb592ab6cae7366f
diego <dcervantesapa@unsa.edu.pe>
2026-05-16 23:01:53 -0500

    implementamos metodo join.py

---
+++ src/join.py
@@ -0,0 +1,6 @@
+from chessPictures import *
+from interpreter import draw
+
+#Juntamos directamente la torre y el caballo en una sola fila horizontal
+resultado_join = rock.join(knight)
+draw(resultado_join)
