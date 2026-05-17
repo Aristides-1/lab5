c437ce98b8055b4b6c467a98543773a4d26bbf3b
diego <dcervantesapa@unsa.edu.pe>
2026-05-16 23:42:52 -0500

    ejecutando b)

--- src/Ejercicio2b.py
+++ src/Ejercicio2b.py
@@ -1,2 +1,13 @@
-from interpreter import draw
 from chessPictures import *
+from interpreter import draw
+
+fila_arriba = knight.join(knight.negative())
+
+# Los caballos de abajo usan el negativo y además se voltean horizontalmente
+caballo_inf_izq = knight.negative().verticalMirror()
+caballo_inf_der = knight.verticalMirror()
+fila_abajo = caballo_inf_izq.join(caballo_inf_der)
+
+resultado = fila_abajo.up(fila_arriba)
+
+draw(resultado)
