2a5785903c7be23c0f902c8220acfab498d464f6
diego <dcervantesapa@unsa.edu.pe>
2026-05-16 23:43:12 -0500

    ejecutando c)

--- src/Ejercicio2c.py
+++ src/Ejercicio2c.py
@@ -1,2 +1,7 @@
-from interpreter import draw
 from chessPictures import *
+from interpreter import draw
+
+#Repetimos la reina 4 veces hacia el costado
+resultado = queen.horizontalRepeat(4)
+
+draw(resultado)
