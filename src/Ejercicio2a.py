e60d3af0ee8a8716e4fb7d4702e1bc21e103f744
diego <dcervantesapa@unsa.edu.pe>
2026-05-16 23:42:23 -0500

    ejecutando a)

--- src/Ejercicio2a.py
+++ src/Ejercicio2a.py
@@ -1,6 +1,13 @@
 from chessPictures import *
 from interpreter import draw
 
-#volteamos el caballo
-caballo_volteado = knight.verticalMirror()
-draw(caballo_volteado)
+# Fila superior: Caballo blanco y Caballo negro
+fila_arriba = knight.join(knight.negative())
+
+# Fila inferior: Caballo negro y Caballo blanco
+fila_abajo = knight.negative().join(knight)
+
+# Armamos el bloque poniendo la de arriba sobre la de abajo
+resultado = fila_abajo.up(fila_arriba)
+
+draw(resultado)
