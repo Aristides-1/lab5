bf11c9828b7ac95d2b409137aaea4782246eabec
diego <dcervantesapa@unsa.edu.pe>
2026-05-16 23:19:30 -0500

    implementamos metodo horizontalRepeat

---
+++ src/horizontalRepeat.py
@@ -0,0 +1,8 @@
+from chessPictures import *
+from interpreter import draw
+
+# Repetimos el peón 4 veces horizontalmente
+resultado_horizontal = pawn.horizontalRepeat(4)
+
+# Dibujamos el experimento
+draw(resultado_horizontal)
