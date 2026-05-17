eda09f780014b8d0199592dae03906bd143aef19
diego <dcervantesapa@unsa.edu.pe>
2026-05-16 23:51:18 -0500

    ejecutando e)

--- src/Ejercicio2e.py
+++ src/Ejercicio2e.py
@@ -1,2 +1,9 @@
-from interpreter import draw
 from chessPictures import *
+from interpreter import draw
+
+square_black = square.negative()
+par_invertido = square_black.join(square)
+
+#Repetimos el patrón de dos casilleros 4 veces para llegar a 8
+resultado = par_invertido.horizontalRepeat(4)
+draw(resultado)
